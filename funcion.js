89f3fe3d87c4287b4247cba6b22999e2e6a43a91
María Espartero García <133367024+maesga1@users.noreply.github.com>
2023-06-22 09:55:54 +0200

    first commit

---
+++ funcion.js
@@ -0,0 +1,15 @@
+function fadeSqu() {
+    document.getElementById("squ").style.visibility = "hidden";
+  }
+function fadeTri() {
+    document.getElementById("tri").style.visibility = "hidden";
+  }
+function fadeCir() {
+    document.getElementById("cir").style.visibility = "hidden";
+  }
+
+  function reset() {
+    document.getElementById("cir").style.visibility = "visible";
+    document.getElementById("tri").style.visibility = "visible";
+    document.getElementById("squ").style.visibility = "visible";
+  }
